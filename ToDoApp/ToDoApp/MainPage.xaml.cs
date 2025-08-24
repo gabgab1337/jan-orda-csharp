ea9fa2bca9ee2f9a9f74054d9a406174b4708788
Gabriel Hanszke <gabrielhanszke@gmail.com>
2025-08-24 17:28:29 +0200

    add: Initial todo collection

--- ToDoApp/ToDoApp/MainPage.xaml.cs
+++ ToDoApp/ToDoApp/MainPage.xaml.cs
@@ -1,63 +1,15 @@
 ﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using ToDoApp.ViewModels;
 
 namespace ToDoApp
 {
-    public partial class MainPage : ContentPage, INotifyPropertyChanged
+    public partial class MainPage : ContentPage
     {
-        private string _newTodoTitle;
-        public string NewTodoTitle
-        {
-            get => _newTodoTitle;
-            set
-            {
-                if (_newTodoTitle != value)
-                {
-                    _newTodoTitle = value;
-                    OnPropertyChanged();
-
-                    OnPropertyChanged(nameof(TitleCharacterCount));
-                    IsAddButtonEnabled = !string.IsNullOrWhiteSpace(value);
-                }
-            }
-        }
-
-        private bool _isAddButtonEnabled;
-        public bool IsAddButtonEnabled
-        {
-            get => _isAddButtonEnabled;
-            set
-            {
-                if(_isAddButtonEnabled != value)
-                {
-                    _isAddButtonEnabled = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
-
-        public string TitleCharacterCount => $"Liczba znaków: {NewTodoTitle?.Length ?? 0}";
-
         public MainPage()
         {
             InitializeComponent();
-            BindingContext = this;
+            BindingContext = new MainViewModel();
         }
-
-        private void OnClearedClicked(object sender, EventArgs e)
-        {
-            NewTodoTitle = string.Empty;
-        }
-
-        #region INotifyPropertyChanged Implementation
-        // Implementujemy interfejs
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        // Tworzymy metodę do eventu
-        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-        #endregion
     }
 }
--- ToDoApp/ToDoApp/ViewModels/MainViewModel.cs
+++ ToDoApp/ToDoApp/ViewModels/MainViewModel.cs
@@ -1,12 +1,53 @@
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Windows.Input;
 using ToDoApp.Models;
 
 namespace ToDoApp.ViewModels
 {
-  public class MainViewModel : INotifyPropertyChanged
-  {
-    public ObservableCollection<ToDoItem> ToDoItems { get; set; } = new ObservableCollection<ToDoItem>();
-  }
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<ToDoItem> ToDoItems { get; set; } = new ObservableCollection<ToDoItem>();
+
+        private string _newTodoTitle;
+        public string NewTodoTitle
+        {
+            get => _newTodoTitle;
+            set
+            {
+                if (_newTodoTitle != value)
+                {
+                    _newTodoTitle = value;
+                    OnPropertyChanged();
+
+                    (AddToDoCommand as Command).ChangeCanExecute();
+                }
+            }
+        }
+
+        public ICommand AddToDoCommand { get; }
+
+        public MainViewModel()
+        {
+            AddToDoCommand = new Command(OnAddToDo, CanAddToDo);
+        }
+
+        private void OnAddToDo()
+        {
+            ToDoItems.Add(new ToDoItem { Title =  NewTodoTitle, IsDone = false });
+            NewTodoTitle = string.Empty;
+        }
+
+        private bool CanAddToDo()
+        {
+            return !string.IsNullOrWhiteSpace(NewTodoTitle);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
 }
