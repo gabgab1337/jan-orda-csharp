b5154ef929254cf7589524de8fb9c5b74e049dcb
Gabriel Hanszke <gabrielhanszke@gmail.com>
2025-08-24 17:49:54 +0200

    fix: singleton errors

--- ToDoApp/ToDoApp/MauiProgram.cs
+++ ToDoApp/ToDoApp/MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using ToDoApp.ViewModels;
 
 namespace ToDoApp
 {
@@ -14,6 +15,9 @@ namespace ToDoApp
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
+
+            builder.Services.AddSingleton<MainViewModel>();
+            builder.Services.AddSingleton<MainPage>();
 
 #if DEBUG
     		builder.Logging.AddDebug();
