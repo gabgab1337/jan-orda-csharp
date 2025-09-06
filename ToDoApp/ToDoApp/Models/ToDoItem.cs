e7e710f7879038f4dc40563cc6f1413e03f5a1b1
Gabriel Hanszke <gabrielhanszke@gmail.com>
2025-09-06 20:03:44 +0200

    add: save/load

--- ToDoApp/ToDoApp/Models/ToDoItem.cs
+++ ToDoApp/ToDoApp/Models/ToDoItem.cs
@@ -1,8 +1,41 @@
-    namespace ToDoApp.Models
+using System.ComponentModel;
+
+namespace ToDoApp.Models
 {
-  public class ToDoItem
+  public class ToDoItem : INotifyPropertyChanged
   {
-    public string Title { get; set; }
-    public bool IsDone { get; set; }
+    private string _title;
+    public string Title
+    {
+      get => _title;
+      set
+      {
+        if (_title != value)
+        {
+          _title = value;
+          OnPropertyChanged(nameof(Title));
+        }
+      }
+    }
+
+    private bool _isDone;
+    public bool IsDone
+    {
+      get => _isDone;
+      set
+      {
+        if (_isDone != value)
+        {
+          _isDone = value;
+          OnPropertyChanged(nameof(IsDone));
+        }
+      }
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    protected void OnPropertyChanged(string propertyName)
+    {
+      PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
   }
 }
--- ToDoApp/ToDoApp/ViewModels/MainViewModel.cs
+++ ToDoApp/ToDoApp/ViewModels/MainViewModel.cs
@@ -3,11 +3,16 @@ using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Windows.Input;
 using ToDoApp.Models;
+using System.Text.Json;
+using System.IO;
+using Microsoft.Maui.Storage;
+using System.Diagnostics;
 
 namespace ToDoApp.ViewModels
 {
     public class MainViewModel : INotifyPropertyChanged
     {
+        private readonly string _saveFilePath = Path.Combine(FileSystem.Current.AppDataDirectory, "todos.json");
         public ObservableCollection<ToDoItem> ToDoItems { get; set; } = new ObservableCollection<ToDoItem>();
 
         // public int ActiveTaskCount => ToDoItems.Count(item => !item.IsDone);
@@ -35,17 +40,51 @@ namespace ToDoApp.ViewModels
         {
             AddToDoCommand = new Command(OnAddToDo, CanAddToDo);
             RemoveCompletedItemsCommand = new Command(RemoveCompletedItems, CanRemoveCompletedItems);
+
+            LoadToDoItems();
+            ToDoItems.CollectionChanged += (s, e) => SaveToDoItems();
         }
 
-        private void OnAddToDo()
+        private void SaveToDoItems()
         {
-            ToDoItems.Add(new ToDoItem { Title =  NewTodoTitle, IsDone = false });
-            NewTodoTitle = string.Empty;
+            var json = JsonSerializer.Serialize(ToDoItems);
+            File.WriteAllText(_saveFilePath, json);
+            Debug.WriteLine("Zapisano");
+        }
+
+        private void LoadToDoItems()
+        {
+            if (File.Exists(_saveFilePath))
+            {
+                var json = File.ReadAllText(_saveFilePath);
+                var items = JsonSerializer.Deserialize<ObservableCollection<ToDoItem>>(json);
+
+                if (items != null)
+                {
+                    ToDoItems.Clear();
+                    foreach (var item in items)
+                    {
+                        item.PropertyChanged += ToDoItem_PropertyChanged;
+                        ToDoItems.Add(item);
+                    }
+                    Debug.WriteLine("Za³adowano");
+                }
+            }
         }
 
         private bool CanAddToDo()
         {
             return !string.IsNullOrWhiteSpace(NewTodoTitle);
+        }
+
+        private void OnDeleteToDo(ToDoItem itemToDelete)
+        {
+            if (itemToDelete != null)
+            {
+                itemToDelete.PropertyChanged -= ToDoItem_PropertyChanged;
+                ToDoItems.Remove(itemToDelete);
+            }
+            SaveToDoItems();
         }
 
         private void RemoveCompletedItems()
@@ -55,6 +94,7 @@ namespace ToDoApp.ViewModels
             {
                 ToDoItems.Remove(item);
             }
+            SaveToDoItems();
         }
 
         private bool CanRemoveCompletedItems()
@@ -62,7 +102,24 @@ namespace ToDoApp.ViewModels
             return ToDoItems.Any(item => item.IsDone);
         }
 
+        private void ToDoItem_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(ToDoItem.IsDone))
+            {
+                (RemoveCompletedItemsCommand as Command).ChangeCanExecute();
+            }
+        }
+
         public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnAddToDo()
+        {
+            var newItem = new ToDoItem { Title = NewTodoTitle, IsDone = false };
+            newItem.PropertyChanged += ToDoItem_PropertyChanged;
+            ToDoItems.Add(newItem);
+            NewTodoTitle = string.Empty;
+            SaveToDoItems();
+        }
         protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
